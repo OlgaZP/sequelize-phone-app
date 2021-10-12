e5bd1688cd5f8c78690485ac512696228edccbc8
Olga K <oskozlova@gmail.com>
2021-10-12 19:14:59 +0300

    add CRUD actions to phoneController

--- controllers/phone.controller.js
+++ controllers/phone.controller.js
@@ -1,33 +1,101 @@
 const { Phone } = require('../models');
 const _ = require('lodash');
 
-module.exports.getPhones = async (req, res) => {
-  console.log(`run getPhones method`);
-};
-
-module.exports.getPhoneById = async (req, res) => {
-  console.log(`run getPhoneById method`);
-};
-
-module.exports.createPhone = async (req, res, next) => {
-  console.log(`run createPhones method`);
-  const { body } = req;
-  console.log(`body`, body);
+module.exports.getPhones = async (req, res, next) => {
   try {
-    const newPhone = await Phone.create(body);
-    console.log(`newPhone.get()`, newPhone.get());
+    const foundPhones = await Phone.findAll({
+      raw: true,
+      attributes: {
+        exclude: ['id', 'createdAt', 'updatedAt'],
+      },
+    });
 
-    res.status(200).send();
+    res.status(200).send({ data: foundPhones });
   } catch (err) {
-    console.log(`err`, err);
     next(err);
   }
 };
 
-module.exports.updatePhone = async (req, res) => {
-  console.log(`run updatePhones method`);
+module.exports.getPhoneById = async (req, res, next) => {
+  const {
+    params: { phoneId },
+  } = req;
+
+  try {
+    const [foundPhones] = await Phone.findAll({
+      raw: true,
+      where: { id: phoneId },
+      attributes: {
+        exclude: ['id', 'createdAt', 'updatedAt'],
+      },
+    });
+
+    if (foundPhones) {
+      return res.status(200).send({ data: foundPhones });
+    }
+    res.status(404).send('Phone not found');
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports.deletePhone = async (req, res) => {
-  console.log(`run deletePhones method`);
+module.exports.createPhone = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const newPhone = await Phone.create(body);
+    const preparedPhone = _.omit(newPhone.get(), [
+      'id',
+      'createdAt',
+      'updatedAt',
+    ]);
+    res.status(200).send(preparedPhone);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updatePhone = async (req, res, next) => {
+  const {
+    params: { phoneId },
+    body,
+  } = req;
+
+  try {
+    const [updatedPhoneCount, [updatedPhone]] = await Phone.update(body, {
+      where: { id: phoneId },
+      returning: true,
+    });
+
+    if (updatedPhoneCount > 0) {
+      const preparedPhone = _.omit(updatedPhone.get(), [
+        'id',
+        'createdAt',
+        'updatedAt',
+      ]);
+      return res.status(200).send({ data: preparedPhone });
+    }
+    res.status(404).send('Phone Not Found');
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports.deletePhone = async (req, res, next) => {
+  const {
+    params: { phoneId },
+  } = req;
+
+  try {
+    const deletedPhonesCount = await Phone.destroy({ where: { id: phoneId } });
+
+    if (deletedPhonesCount > 0) {
+      return res
+        .status(200)
+        .send(`Delete ${deletedPhonesCount} phones successfull`);
+    }
+    res.status(404).send('Phone not found');
+  } catch (err) {
+    next(err);
+  }
 };
